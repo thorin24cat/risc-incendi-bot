1974083519a7dfb95e107f0e0fa8f3378d616ab0
thorin24cat <thorin24@gmail.com>
2026-06-23 00:04:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ from telegram.ext import (
 )
 import requests
 import os
-
+import json
 PLA_ALFA_URL = (
     "https://services7.arcgis.com/"
     "ZCqVt1fRXwwK6GF4/arcgis/rest/services/"
     "Pla_Alfa_Municipal_Avui_FL_alternatiu_VW/"
     "FeatureServer/0/query"
 )
-
+USUARIOS_FILE = "usuarios.json"
 
 def texto_nivel(nivel):
     textos = {
@@ -27,7 +27,17 @@ def texto_nivel(nivel):
     }
     return textos.get(nivel, "Nivell desconegut")
 
+def cargar_usuarios():
+    try:
+        with open(USUARIOS_FILE, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except:
+        return {}
 
+
+def guardar_usuarios(datos):
+    with open(USUARIOS_FILE, "w", encoding="utf-8") as f:
+        json.dump(datos, f, ensure_ascii=False, indent=2)
 def consultar_pla_alfa(municipio):
     params = {
         "where": f"NOMMUNI='{municipio}'",
@@ -80,7 +90,36 @@ async def olivella(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text(
             f"Error: {str(e)}"
         )
+async def estado(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
+    usuarios = cargar_usuarios()
+
+    user_id = str(update.effective_user.id)
+
+    municipio_usuario = None
+
+    if user_id in usuarios:
+        municipio_usuario = usuarios[user_id].get("municipio")
+
+    olivella = consultar_pla_alfa("Olivella")
+
+    mensaje = (
+        f"📍 Olivella\n"
+        f"🔥 Pla Alfa: Nivel {olivella['nivel']}\n\n"
+    )
+
+    if municipio_usuario:
+
+        actual = consultar_pla_alfa(municipio_usuario)
+
+        if actual:
+            mensaje += (
+                f"📍 Tu ubicación guardada: "
+                f"{actual['municipio']}\n"
+                f"🔥 Pla Alfa: Nivel {actual['nivel']}"
+            )
+
+    await update.message.reply_text(mensaje)
 
 async def location(update: Update, context: ContextTypes.DEFAULT_TYPE):
     lat = update.message.location.latitude
@@ -117,7 +156,13 @@ async def location(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 "No se ha podido determinar el municipio."
             )
             return
+        usuarios = cargar_usuarios()
 
+usuarios[str(update.effective_user.id)] = {
+    "municipio": municipio
+}
+
+guardar_usuarios(usuarios)
         resultado = consultar_pla_alfa(municipio)
 
         if resultado is None:
@@ -147,7 +192,7 @@ def main():
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("olivella", olivella))
     app.add_handler(MessageHandler(filters.LOCATION, location))
-
+    app.add_handler(CommandHandler("estado", estado))
     app.run_polling()
 
 
